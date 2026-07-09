26b91b325914da099bb738c5ae5545fd3e170d48
Max Zimmermann <115488694+Maxzimmerman@users.noreply.github.com>
2026-07-09 16:48:12 +0800

    feat: added check hash functions.

--- lib/presence_lab/users/_index.ex
+++ lib/presence_lab/users/_index.ex
@@ -11,4 +11,31 @@ defmodule Users do
     User.changeset(%User{}, params)
     |> Repo.insert()
   end
+
+  def create_user(_) do
+    Logger.error("username and password required to create user: #{__MODULE__}.create_user/1")
+
+    {:error, :invalid_input}
+  end
+
+  def check_password(username, password) do
+    with %User{password_hash: hash} <- get_user(username),
+         {:success, :valid_hash} <- check_hash(password, hash) do
+      {:ok, :valid}
+    end
+  end
+
+  def get_user(username) do
+    Repo.get_by(User, username: username)
+  end
+
+  def check_hash(password, hash) do
+    case Bcrypt.verify_pass(password, hash) do
+      false ->
+        {:error, :invalid_hash}
+
+      true ->
+        {:success, :valid_hash}
+    end
+  end
 end
